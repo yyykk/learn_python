328b41b3f875ba07071142f5a594d38f7c095539
yyykk <yukaieecsu@gmail.com>
2016-12-18 23:48:21 +0800

    hasty demo

---
+++ demo.py
@@ -0,0 +1,55 @@
+#str = "Hello World!"
+#str2 = str + "xixi"
+#print(str[:3])
+#print(str2)
+
+xixi = (1, 2, 3, 4, "end")
+
+
+#for num in range(0,4):
+#    print(xixi[num])
+
+#dict = {}
+
+#dict[1] = "one"
+#dict[2] = "two"
+#dict["xixi"] = "xixi"
+
+#count = 1
+#while (count < 3):
+#    print(dict[count])
+#    count += 1
+
+#count = -1
+#if (count):
+#    pass
+#else:
+#    print(count)
+
+
+#for count in range(0,5):
+#    print(xixi[count])
+    #count += 1
+#else:
+    #print(xixi[count])
+
+you = lambda a: a + "you"
+
+def sum(a, b):
+    return a + b
+    
+print(you("love "))
+print(sum(1, 4.44))
+
+a = input()
+
+
+
+
+
+
+
+
+
+
+
