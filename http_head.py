26b0a8f96a72b86d58a06b2e522f9d17ac2c4a8a
yyykk <yukaieecsu@gmail.com>
2016-12-23 23:39:57 +0800

    select ignore and key word

--- http_head.py
+++ http_head.py
@@ -1,26 +1,47 @@
 import pymysql
+import time
 
-database = pymysql.connect (host = "localhost", user = "root", passwd = "mysql", db = "demo")
-cursor = database.cursor()
+#database = pymysql.connect (host = "localhost", user = "root", passwd = "mysql", db = "demo")
+#cursor = database.cursor()
+
+start_time = time.time()
 file_error = open("http_error.txt", "wb+")
 file_r = open("MS_S1_U_Http_0711_gaosu.txt", 'rb')
-title = file_r.readline()
-title = title.decode('replace')
+file_http = open("http.txt", "wb+")
 
-title = title.strip('\n')
-title += ","
-table_head = []
-head = ""
-for string_iter in range (len(title)):
-    if ( title[string_iter] != "," ):
-        head += title[string_iter]
-    else:
-        table_head.append(head)
-        head = ""
-
-cursor.execute("drop table http_test")
-
+ignore_word = ["RAT", "MachineIPAddType", "TAC", "AppTypeCode", "USER_IPv4", 
+               "UserPort", "L4Protocal", "ULIPFRAGPACKETS", "DLIPFRAGPACKETS", 
+               "WindowSize", "MSSSize", "TCPConnStatus", "SessionIsEnd",
+               "URI", "XOnlineHost", "UserAgent", "Cookie", "PortalAppCollection"]
 key_word = ["XDRID","RequestTime","ProcedureEndTime"]
+
+title = file_r.readline()
+title = title.decode(encoding='utf-8',errors='ignore')
+
+title = title.strip('\r\n')
+title += ","
+title = "," + title
+table_head = []
+order_list = []#begin 0
+key_list = []
+head = ""
+order = 0
+second = 0
+while(1):
+    first = title.find(",", second) + 1
+    second = title.find(",", first + 1)
+    head = title[first : second]
+    if (not(head in ignore_word)):
+        order_list.append(order)
+        table_head.append(head)
+    if (head in key_word):
+        key_list.append(order)
+    order += 1
+    if(second == len(title) - 1):
+        break;
+#cursor.execute("drop table http_test")
+
+'''
 long_title = ["HOST","HTTP_content_type"]
 for key_iter in range (len(key_word)):
     if(key_iter == 0):
@@ -41,11 +62,87 @@ for table_iter in range (len(table_head)):
         cursor.execute("alter table http_test modify " + table_head[table_iter] + " char(30) not null;")
 
 cursor.execute("alter table http_test add constraint pk_orderinfo PRIMARY KEY" + key_word_string)
+'''
+
+def delete_comma (temp):
+    comma_count = 0
+    comma_index = -1
+    while(1):
+        comma_index = temp.find(",", comma_index + 1)
+        comma_count += 1;
+        if(comma_count == 53):
+            temp = temp[0:comma_index] + temp[comma_index + 1 : len(temp)]
+            break
+    return temp
+'''
+    for code_count in range (len(temp)):
+        if (temp[code_count] == ","):
+            comma_count += 1
+            if (comma_count == 53):
+                temp = temp[0:code_count] + temp[code_count + 1 : len(temp)]
+                break
+'''
+    
+
+string = ""
+counter = 0
+word = ""
+while (1):    
+    string0 = string
+    word_list = []
+    
+    while(1):
+        string = file_r.readline()
+        try:
+            string = string.decode(encoding='utf-8', errors='ignore')
+        except Exception as e:
+            file_error.write(bytes(repr(e) + '\n', 'utf-8'))
+            print("error = ", counter)
+        if (string != string0):
+            break
+    if(string.count(",") != 60):
+        string = delete_comma(string)
+    if(string == ""):
+        break
+    string = string.strip('\n')
+    string += ","
+    second = string.find(",")
+    first = 0
+    order = 0
+    word_command = ""
+    while(1):
+        word = string[first + 1 : second]
+        if (order in order_list):
+            word_list.append(word)
+            word = ""
+        if(second == len(string) - 1):
+            break;
+        first = string.find(",", first + 1)
+        second = string.find(",", second + 1)
+        order += 1
+        
+    for word_list_iter in range(len(word_list)):
+        word_command += word_list[word_list_iter] + ","
+    else:
+        word_command = word_command.strip(',')
+    try:
+        file_http.write(bytes(word_command + '\n', 'utf-8'))
+    except Exception as e:
+        file_error.write(bytes(repr(e) + '\n', 'utf-8'))
+        print("error = ", counter)    
+    counter += 1
+    if (counter % 100000 == 0):
+        print("right = ", counter)
+        print(time.time() - start_time)
+    if (counter == 10000000):
+        print("pass = ", counter)
+        break
 
 
+'''
 add_list = []
 string = file_r.readline()
-string = string.decode('replace')
+string = string.decode(encoding='utf-8',errors='ignore')
 counter = 0
 while (((string) != "")):
     string0 = string;
@@ -71,7 +168,7 @@ while (((string) != "")):
         print(counter)
     while(string == string0):
         string = file_r.readline()
-        string = string.decode('replace')
+        string = string.decode(encoding='utf-8',errors='ignore')
     counter += 1
     
     if(counter == 100000):
@@ -81,8 +178,10 @@ cursor.execute("select * from http_test")
 data = cursor.fetchall()
 #data = cursor.fetchone()
 print("counter = ", counter, "\t", "len(data) = ", len(data))
-
 database.close()
+'''
 file_r.close()
+file_http.close()
 file_error.close()
-print("success")
+print(time.time() - start_time)
+print("table_head = ", table_head)
--- mme_head.py
+++ mme_head.py
@@ -1,36 +1,41 @@
 import pymysql
-
+import time
 #database = pymysql.connect (host = "localhost", user = "root", passwd = "mysql", db = "demo")
 #cursor = database.cursor()
 
+start_time = time.time()
 file_r = open("MS_S1_MME_0711_gaosu.txt", 'rb')
 file_error = open("mme_error.txt", "wb+")
+file_mme = open("mme.txt", "wb+")
 
-ignore_word = ["Interface","RAT"]
+ignore_word = ["Interface", "RAT", "RequestCause", "FailureCause", "MMEUES1APID", "OldMTMSI", "MMEGroupID", "TMSI", "MTMSI", "UserIPv4", "TAC", "APN\r"]
 key_word = ["XDRID", "RequestTime"]
 
 title = file_r.readline()
-title = title.decode(encoding='utf-8',errors='repalce')
+title = title.decode(encoding='utf-8',errors='ignore')
 
 title = title.strip('\n')
 title += ","
+title = "," + title
 table_head = []
 order_list = []#begin 0
 key_list = []
 head = ""
 order = 0
-for string_iter in range (len(title)):
-    if ( title[string_iter] != "," ):
-        head += title[string_iter]
-    else:
-        if (not(head in ignore_word)):
-            order_list.append(order)
-            table_head.append(head)
-        if (head in key_word):
-            key_list.append(order)
-        order += 1
-        head = ""
-
+second = 0
+while(1):
+    first = title.find(",", second) + 1
+    second = title.find(",", first + 1)
+    head = title[first : second]
+    if (not(head in ignore_word)):
+        order_list.append(order)
+        table_head.append(head)
+    if (head in key_word):
+        key_list.append(order)
+    order += 1
+    if(second == len(title) - 1):
+        break;
+        
 #try:
 #    cursor.execute("drop table mme_test")
 #except Exception as e:
@@ -58,42 +63,58 @@ for table_iter in range (len(table_head)):
 
 cursor.execute("alter table mme_test add constraint pk_orderinfo PRIMARY KEY" + key_word_string)
 '''
-string = file_r.readline()
-string = string.decode(encoding='utf-8',errors='repalce')
+
+
+string = ""
 counter = 0
-add_list = []
 word = ""
-while (((string) != "")):
+while (1):
     order = 0
     word_command = ""
-    key = ""
-    word_list = []
     string0 = string
-    for string_iter in range (len(string)):
-        if ( string[string_iter] != "," ):
-            word += string[string_iter]
-        else:
-            if (order in order_list):
-                word_list.append(word)
-            if (order in key_list):
-                key += word
-            order += 1
+    word_list = []
+    
+    while(1):
+        string = file_r.readline()
+        try:
+            string = string.decode(encoding='utf-8', errors='ignore')
+        except Exception as e:
+            file_error.write(bytes(repr(e) + '\n', 'utf-8'))
+            print("error = ", counter)
+        if (string != string0):
+            break
+    #string = string.strip('\n')
+    if(string == ""):
+        break
+    string = string.strip('\n')
+    string += ","
+    second = string.find(",")
+    first = 0
+    while(1):
+        word = string[first + 1 : second]
+        if (order in order_list):
+            word_list.append(word)
             word = ""
-    if (key in key_list):
-        continue
-    else:
-        add_list.append(key)
+        first = string.find(",", first + 1)
+        second = string.find(",", second + 1)
+        order += 1
+        if(second == len(string) - 1):
+            break;
     for word_list_iter in range(len(word_list)):
         word_command += word_list[word_list_iter] + ","
     else:
         word_command = word_command.strip(',')
-    word_command = word_command.replace(",", "','")
-    #print(word_command)
-    while(string == string0):
-        string = file_r.readline()
-        string = string.decode(encoding='utf-8', errors='repalce')
+    try:
+        pass
+        file_mme.write(bytes(word_command + '\n', 'utf-8'))
+    except Exception as e:
+        file_error.write(bytes(repr(e) + '\n', 'utf-8'))
+        print("error = ", counter)    
     counter += 1
-    if (counter == 1000000):
+    if (counter % 1000000 == 0):
+        print("right = ", counter)
+    if (counter == 11000000):
+        print("pass = ", counter)
         break
 
     
@@ -130,7 +151,9 @@ data = cursor.fetchall()
 print("counter = ", counter, "\t", "len(data) = ", len(data))
 
 database.close()
-'''t
+'''
 file_r.close()
+file_mme.close()
 file_error.close()
-print("success")
+print("spend time = ", time.time() - start_time)
+print("table_head = ", table_head)
